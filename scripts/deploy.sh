65c135f17adb29ef1fea45f011995559cf1b7f80
mimijae <qzqzcaraz00@gmail.com>
2025-07-27 17:04:23 +0900

    docker-compose.yml에서 이미지 경로를 간소화하고, deploy.sh에서 환경변수 설정을 로컬 빌드용으로 수정하여 배포 스크립트를 개선함

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -55,9 +55,7 @@ export VERSION=$VERSION
 export BUILD_TIME=$BUILD_TIME
 export REACT_APP_DEPLOYMENT_TYPE=$TARGET_ENV
 
-# 환경변수 설정
-export REGISTRY=${REGISTRY:-ghcr.io}
-export IMAGE_NAME=${IMAGE_NAME:-cozyrim/image-change}
+# 환경변수 설정 (로컬 빌드용)
 export VERSION=${VERSION:-latest}
 
 docker-compose build app-$TARGET_ENV_LOWER
